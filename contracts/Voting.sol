afeb019ad6ec22d9e52dc5916b64d271463fabfd
sanjay-in <sanjaysridharan@outlook.com>
2024-07-25 18:56:46 +0530

    format contract

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,83 +2,158 @@
 pragma solidity ^0.8.24;
 
 contract Voting {
-
     ///////////////////////
-   // Type Declaration  //
-  ///////////////////////
-
-  struct Candidate {
-    uint256 id;
-    string name;
-    string party;
-    string image;
-    uint256 count;
-  }
-
+    // Type Declaration  //
     ///////////////////////
-   // State Variables   //
-  ///////////////////////
 
-  Candidate[] private s_candidates;
-  Candidate private s_winner;
-  mapping (address => bool) s_hasVoted;
-  uint256 private immutable i_startTime;
-  uint256 private immutable i_endTime;
-
-    ///////////////////////
-   // Events            //
-  ///////////////////////
-
-  event VoteCasted(address indexed voter, uint256 indexed id, string name, string party);
-  event WinnerSelected(uint256 indexed id, string name, string party, string image, uint256 count);
-  event PollOpened();
-
-    ///////////////////////
-   // Errors            //
-  ///////////////////////
-
-  error Voting__PollClosed();
-  error Voting__PollNotOpen();
-  error Voting__AlreadyCastedVote();
-
-    ///////////////////////
-   // Modifiers         //
-  ///////////////////////
-
-  modifier pollOpen() {
-    if (block.timestamp >= i_endTime) {
-      revert Voting__PollClosed();
+    struct Candidate {
+        uint256 id;
+        string name;
+        string party;
+        string image;
+        uint256 count;
     }
-    if (block.timestamp < i_startTime) {
-      revert Voting__PollNotOpen();
-    }
-    _;
-  }
 
     ///////////////////////
-   // Functions         //
-  ///////////////////////
+    // State Variables   //
+    ///////////////////////
 
-  constructor(Candidate[] memory candidates, uint256 startTime, uint256 endTime) {
-    for(uint256 i=0; i < candidates.length; i++) {
-      uint256 candidateId = i+1;
-      s_candidates.push(Candidate(
-        candidateId,
-        candidates[i].name,
-        candidates[i].party,
-        candidates[i].image,
-        0
-      ));
+    Candidate private s_winner;
+    mapping(uint256 => Candidate) s_candidates;
+    mapping(address => bool) s_hasVoted;
+    uint256 private immutable i_startTime;
+    uint256 private immutable i_endTime;
+    uint256 private immutable i_candidatesCount;
+
+    ///////////////////////
+    // Events            //
+    ///////////////////////
+
+    event VoteCasted(
+        address indexed voter,
+        uint256 indexed id,
+        string name,
+        string party
+    );
+    event WinnerSelected(
+        uint256 indexed id,
+        string name,
+        string party,
+        string image,
+        uint256 count
+    );
+    event PollOpened();
+
+    ///////////////////////
+    // Errors            //
+    ///////////////////////
+
+    error Voting__PollClosed();
+    error Voting__PollNotOpen();
+    error Voting__AlreadyCastedVote();
+
+    ///////////////////////
+    // Modifiers         //
+    ///////////////////////
+
+    modifier pollOpen() {
+        if (block.timestamp >= i_endTime) {
+            revert Voting__PollClosed();
+        }
+        if (block.timestamp < i_startTime) {
+            revert Voting__PollNotOpen();
+        }
+        _;
     }
-    i_endTime = endTime;
-    i_startTime = startTime;
-  }
 
-  function castVote() external {}
+    modifier alreadyVoted(address sender) {
+        if (s_hasVoted[sender]) {
+            revert Voting__AlreadyCastedVote();
+        }
+        _;
+    }
 
-  function getStartAndEndTime() external view returns (uint256, uint256) {}
+    ///////////////////////
+    // Functions         //
+    ///////////////////////
 
-  function getCandidatesDetails() external view returns (Candidate[] memory) {}
+    /**
+     * Constructor loops through the candidates array and maps id to candidate info
+     * and sets the total candidates count
+     * @param candidates gets an array of Candidate[] (struct)
+     * @param startTime start time of the contract
+     * @param endTime ending time of the contract
+     */
+    constructor(
+        Candidate[] memory candidates,
+        uint256 startTime,
+        uint256 endTime
+    ) {
+        for (uint256 i = 0; i < candidates.length; i++) {
+            uint256 candidateId = i + 1;
+            s_candidates[candidateId] = Candidate(
+                candidateId,
+                candidates[i].name,
+                candidates[i].party,
+                candidates[i].image,
+                0
+            );
+        }
+        i_endTime = endTime;
+        i_startTime = startTime;
+        i_candidatesCount = candidates.length;
+    }
 
-  function getIsVotedCandidate() external view returns (bool) {}
+    /**
+     * Checks if the poll is open and voter has already voted
+     * Maps the user address to voted and emits event
+     * @param _candidateId ID of the candidate to cast vote for
+     */
+    function castVote(
+        uint256 _candidateId
+    ) external pollOpen alreadyVoted(msg.sender) {
+        s_hasVoted[msg.sender] = true;
+        s_candidates[_candidateId].count += 1;
+        Candidate memory votedForCandidate = s_candidates[_candidateId];
+        emit VoteCasted(
+            msg.sender,
+            _candidateId,
+            votedForCandidate.name,
+            votedForCandidate.party
+        );
+    }
+
+    /**
+     * Returns start and end time of the contract
+     * @return i_startTime
+     * @return i_endTime
+     */
+    function getStartAndEndTime() external view returns (uint256, uint256) {
+        return (i_startTime, i_endTime);
+    }
+
+    /**
+     * Checks if poll is open and returns the details for all candidates
+     * @return allCandidates array of Candidate[]
+     */
+    function getCandidatesDetails()
+        external
+        view
+        pollOpen
+        returns (Candidate[] memory)
+    {
+        Candidate[] memory allCandidates;
+        for (uint i = 0; i < i_candidatesCount; i++) {
+            allCandidates[i] = (s_candidates[i]);
+        }
+        return allCandidates;
+    }
+
+    /**
+     * Checks if poll open and returns if the voter has already casted vote
+     * @return boolean
+     */
+    function getIsVotedCandidate() external view pollOpen returns (bool) {
+        return s_hasVoted[msg.sender];
+    }
 }
