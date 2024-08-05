8810a1b29e7b2f8df79bbed320a36d61da76fb8e
sanjay-in <sanjaysridharan@outlook.com>
2024-08-05 14:55:22 +0530

    update home component

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import poster1 from "../../assets/poster1.jpg";
+import poster2 from "../../assets/poster2.jpg";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home-component">
+      <h2 className="home-header">Decentralized Voting</h2>
+      <div className="posters">
+        <img className="poster-1" src={poster1} />
+        <img className="poster-2" src={poster2} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
