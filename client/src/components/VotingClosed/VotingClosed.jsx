616309843ea668f1014e89ae0504179f09885576
sanjay-in <sanjaysridharan@outlook.com>
2024-08-29 22:07:36 +0530

    add before after voting time screen

--- client/src/components/VotingClosed/VotingClosed.jsx
+++ client/src/components/VotingClosed/VotingClosed.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import CandidateCard from "../Candidates/CandidateCard/CandidateCard";
+import { countdownClock } from "../CountdownClock/CountdownClock";
+import "./VotingClosed.css";
 
-const VotingClosed = ({ pollEnded, winner }) => {
-  const { image, party, id, name } = winner;
+const VotingClosed = ({ pollEnded, winner, startTime }) => {
+  // const { image, party, id, name } = winner;
+  const [days, hours, minutes, seconds] = countdownClock(startTime);
   return (
     <div>
       {!pollEnded ? (
-        <h2>The poll starts in: 00h:00mins:23secs</h2>
+        <h2 className="voting-start">
+          Poll starts in{" "}
+          <span>
+            {days} days : {hours} hrs : {minutes} mins : {seconds} secs
+          </span>
+        </h2>
       ) : (
         <div>
           <h2>The poll has ended</h2>
           <div>The winner is</div>
-          <CandidateCard image={image} party={party} id={id} name={name} />
+          {/* <CandidateCard image={image} party={party} id={id} name={name} /> */}
         </div>
       )}
     </div>
