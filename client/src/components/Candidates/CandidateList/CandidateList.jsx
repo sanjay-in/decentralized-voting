bef9e7d4e66fe761103ee79cc64c50165b6914f9
sanjay-in <sanjaysridharan@outlook.com>
2024-09-05 23:14:54 +0530

    update candidate list

--- client/src/components/Candidates/CandidateList/CandidateList.jsx
+++ client/src/components/Candidates/CandidateList/CandidateList.jsx
@@ -1,43 +1,16 @@
 import React from "react";
 import CandidateCard from "../CandidateCard/CandidateCard";
 import "./CandidateList.css";
+import Voted from "../../Modals/Voted/Voted";
 
-const CandidateList = () => {
-  const candidatesList = [
-    {
-      id: 1,
-      name: "Joe Biden",
-      party: "Democratic Party",
-      image: "https://blockgeeks.com/wp-content/uploads/2022/10/image4.png.webp",
-      count: 0,
-    },
-    {
-      id: 2,
-      name: "Donald Trump",
-      party: "Republican Party",
-      image: "https://blockgeeks.com/wp-content/uploads/2022/10/image1-2.png.webp",
-      count: 0,
-    },
-    {
-      id: 1,
-      name: "Joe Biden",
-      party: "Democratic Party",
-      image: "https://blockgeeks.com/wp-content/uploads/2022/10/image4.png.webp",
-      count: 0,
-    },
-    {
-      id: 2,
-      name: "Donald Trump",
-      party: "Republican Party",
-      image: "https://blockgeeks.com/wp-content/uploads/2022/10/image1-2.png.webp",
-      count: 0,
-    },
-  ];
+const CandidateList = ({ candidates }) => {
   return (
     <div className="candidate-list">
-      {candidatesList.map(({ image, id, party, count, name }) => {
-        return <CandidateCard image={image} key={id} id={id} party={party} count={count} name={name} />;
-      })}
+      {candidates &&
+        candidates.map(({ image, id, party, count, name }) => {
+          return <CandidateCard image={image} key={id} id={id} party={party} count={count} name={name} />;
+        })}
+      <Voted show={false} />
     </div>
   );
 };
