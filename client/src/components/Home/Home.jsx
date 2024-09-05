ff6da3a563cd004d05703997d503179255b51b31
sanjay-in <sanjaysridharan@outlook.com>
2024-09-05 11:29:58 +0530

    move countdown timer to new component

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,25 +1,20 @@
 import React from "react";
 import poster1 from "../../assets/poster1.jpg";
 import poster2 from "../../assets/poster2.jpg";
-import "./Home.css";
 import CandidateList from "../Candidates/CandidateList/CandidateList";
-import { countdownClock } from "../CountdownClock/CountdownClock";
+import Timer from "../Timer/Timer";
+import "./Home.css";
 
 const Home = () => {
-  const [days, hours, minutes, seconds] = countdownClock(1725992999000);
   return (
     <div className="home-component">
       <h2 className="home-header">Decentralized Voting</h2>
-      <div className="timer">
-        <div className="timer-message">Poll closes in</div>
-        {days} days : {hours} hrs : {minutes} mins : {seconds} secs
-      </div>
+      <Timer />
       <div className="posters">
         <img className="poster-1" src={poster1} />
         <img className="poster-2" src={poster2} />
       </div>
       <CandidateList />
-      {/* {console.log(`${days} days:${hours} hrs:${minutes} mins:${seconds} secs`)} */}
     </div>
   );
 };
