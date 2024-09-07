644bb60697d4157ddd93f284c0a60fd239ff90ce
sanjay-in <sanjaysridharan@outlook.com>
2024-09-07 21:07:53 +0530

    add hasVoted to candidate list

--- client/src/components/Candidates/CandidateList/CandidateList.jsx
+++ client/src/components/Candidates/CandidateList/CandidateList.jsx
@@ -3,12 +3,12 @@ import CandidateCard from "../CandidateCard/CandidateCard";
 import "./CandidateList.css";
 import Voted from "../../Modals/Voted/Voted";
 
-const CandidateList = ({ candidates }) => {
+const CandidateList = ({ candidates, hasVoted }) => {
   return (
     <div className="candidate-list">
       {candidates &&
         candidates.map(({ image, id, party, count, name }) => {
-          return <CandidateCard image={image} key={id} id={id} party={party} count={count} name={name} />;
+          return <CandidateCard image={image} key={id} id={id} party={party} count={count} name={name} hasVoted={hasVoted} />;
         })}
       <Voted show={false} />
     </div>
